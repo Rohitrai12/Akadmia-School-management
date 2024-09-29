ffcab376149873cb6d127c64de571178f396220e
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2024-09-29 15:31:03 +0500

    code update

--- frontend/src/pages/admin/HireTeachers/Hire.jsx
+++ frontend/src/pages/admin/HireTeachers/Hire.jsx
@@ -1,7 +1,7 @@
 const Hire = () => {
-    return ( 
-        <h1 className="text-5xl text-red-300">Hire Teachers</h1>
-     );
+    return (
+        <h1 className="text-4xl font-semibold">Hire Teachers - Under Development - I am working on this Feature</h1>
+    );
 }
- 
+
 export default Hire;
